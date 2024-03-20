8293e1e6ccefee0c11ac78f0e59797243d6afa0c
Joel Shadbolt <joelshadbolt123@hotmail.com>
2024-03-21 01:30:56 +1000

    updating branch protector

--- branch-protector/branch-protect.sh
+++ branch-protector/branch-protect.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 ALLOWED_ACTOR=$1
+SOURCE_BRANCH=$2
+TARGET_BRANCH=$3
 
-if [ "$GITHUB_ACTOR" != "$ALLOWED_ACTOR" ]; then
-  echo "Error: Only $ALLOWED_ACTOR is allowed to push or create a PR on this branch."
-  exit 1
+# Check for actor restrictions if allowed_actor is provided
+if [ -n "$ALLOWED_ACTOR" ]; then
+    if [ "$GITHUB_ACTOR" != "$ALLOWED_ACTOR" ]; then
+        echo "Error: Only $ALLOWED_ACTOR is allowed to push or create a PR."
+        exit 1
+    fi
+fi
+
+# Check for branch merge restrictions if source_branch and target_branch are provided
+if [ -n "$SOURCE_BRANCH" ] && [ -n "$TARGET_BRANCH" ]; then
+    if [ "$GITHUB_BASE_REF" == "$TARGET_BRANCH" ] && [ "$GITHUB_HEAD_REF" == "$SOURCE_BRANCH" ]; then
+        echo "Error: Merging from $SOURCE_BRANCH to $TARGET_BRANCH is not allowed."
+        exit 1
+    fi
 fi
